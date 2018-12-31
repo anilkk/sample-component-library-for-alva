b4f402ce422df6fd777d3ed61731a777e1fa3807
Anil Kumar krishanshetty <anilbms75@gmail.com>
2018-12-31 21:02:14 +0100

    sample component library initial commit

---
+++ src/hello-world.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as Styles from "./hello-world.styled";
+
+export interface HelloWorldProps {
+  color?: Styles.HelloWorldColors;
+  children?: React.ReactNode;
+}
+
+export const HelloWorld: React.SFC<HelloWorldProps> = props => {
+  return (
+    <Styles.StyledHelloWorld color={props.color}>
+      {props.children || "Hello, World!"}
+    </Styles.StyledHelloWorld>
+  );
+};
